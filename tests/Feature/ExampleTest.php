99edd7f4f1e62a97f108e2827cd688da3aca0db2
Kuroganeno1 <111863227+Kuroganeno1@users.noreply.github.com>
2022-11-28 12:23:19 +0100

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,10 +12,55 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+
+
+
+    //          -----------------------         Route Testing       ----------------------------
+    // Makes Clear that every Route has its own Path over the Get-Mehtod of Navigation. 
+    // Hands over the status-code 200 which means the path is existing. Fails with the Status Code 404 .- not found. 
+
+
+
+    public function ShouldFindRouteWork ()
     {
-        $response = $this->get('/');
+        $response = $this->get('/work');
 
         $response->assertStatus(200);
     }
+
+    public function ShouldFindRouteLiving ()
+    {
+        $response = $this->get('/living');
+
+        $response->assertStatus(200);
+    }
+
+    public function ShouldFindRouteDS ()
+    {
+        $response = $this->get('/driving-service');
+
+        $response->assertStatus(200);
+    }
+
+    public function ShouldFindRouteContact ()
+    {
+        $response = $this->get('/contact');
+
+        $response->assertStatus(200);
+    }
+
+    public function ShouldFindRouteCC ()
+    {
+        $response = $this->get('/care-center');
+
+        $response->assertStatus(200);
+    }
+
+    public function ShouldFindRouteAdmin ()
+    {
+        $response = $this->get('/admin');
+
+        $response->assertStatus(200);
+    }
+
 }
