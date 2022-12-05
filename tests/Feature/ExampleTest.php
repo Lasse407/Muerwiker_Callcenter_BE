c5930c2ea152d4c7794d4c163734588f83b18d39
Kuroganeno1 <111863227+Kuroganeno1@users.noreply.github.com>
2022-12-05 12:34:28 +0100

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,13 +13,9 @@ class ExampleTest extends TestCase
      * @return void
      */
 
-
-
-    //          -----------------------         Route Testing       ----------------------------
-    // Makes Clear that every Route has its own Path over the Get-Mehtod of Navigation. 
-    // Hands over the status-code 200 which means the path is existing. Fails with the Status Code 404 .- not found. 
-
-
+    //          -----------------------         Route Testing   (HTTP TESTING)    ----------------------------
+    // Makes Clear that every Route has its own Path over the Get-Method of Navigation.
+    // Hands over the status-code 200 which means the path is existing. Fails with the Status Code 404 .- not found
 
     public function ShouldFindRouteWork ()
     {
@@ -63,4 +59,136 @@ class ExampleTest extends TestCase
         $response->assertStatus(200);
     }
 
+    //          -----------------------         DB Testing       ----------------------------
+    // First two Tests handles small actions within the Database 
+    // These got copied for the remaining Tables within the Database to prove the same thing.
+    // Third Test tests proves the ability to delete curtain users from DB
+
+    public function userExists()
+    {
+        $this->assertDatabaseHas('users', [
+            'name' => 'Thomas Stengel'
+
+        ]);
+    }
+
+    public function contactExists()
+    {
+        $this->assertDatabaseHas('contacts', [
+            'surname' => 'Bork'
+
+        ]);
+    }
+
+    public function PasswordResetTest()
+    {
+        $this->assertDatabaseHas('password_resets', [
+            'email' => 'bork@muerwiker-gruppe.de'
+
+        ]);
+    }
+
+    public function worksExists1()
+    {
+        $this->assertDatabaseHas('works', [
+            'section' => 'Finanzen'
+
+        ]);
+    }
+
+    public function worksExists2()
+    {
+        $this->assertDatabaseHas('works', [
+            'section' => 'Geschäftsleitung'
+
+        ]);
+    }
+
+    public function worksExists3()
+    {
+        $this->assertDatabaseHas('works', [
+            'section' => 'Marketing'
+
+        ]);
+    }
+
+    public function worksExists4()
+    {
+        $this->assertDatabaseHas('works', [
+            'section' => 'Bildung und Begleitung'
+
+        ]);
+    }
+
+    public function livingsExists1()
+    {
+        $this->assertDatabaseHas('living', [
+            'street' => 'Buchenweg'
+
+        ]);
+    }
+
+    public function livingsExists2()
+    {
+        $this->assertDatabaseHas('living', [
+            'street' => 'Terassenstraße'
+
+        ]);
+    }
+
+    public function livingsExists3()
+    {
+        $this->assertDatabaseHas('living', [
+            'street' => 'Nova'
+
+        ]);
+    }
+
+    public function livingsExists4()
+    {
+        $this->assertDatabaseHas('living', [
+            'street' => 'Raiffeisenstraße'
+
+        ]);
+    }
+
+    public function care_centerExists()
+    {
+        $this->assertDatabaseHas('care_centers', [
+            'street' => 'Buchenweg'
+
+        ]);
+    }
+
+    public function userIsMissing()
+    {
+        $this->assertDatabaseMissing('users', [
+            'name' => 'Jay Rehder'
+        ]);
+    }
+
+    public function deleteUser ()
+    {
+        $user = User::factory()->count(1)->make();
+
+        $user = User::first();
+
+        if($user)
+        {
+            $user->delete();
+
+        }
+
+        $this->assertTrue(True);
+    }
+
+    
+    //          -----------------------         Seeder Testing       ----------------------------
+    //
+
+
+    public function testIfSeedersWorks()
+    {
+        $this->seed();
+    }
 }
